21af8e52074eb24dfce1745d4de1b5a76f5a0d2a
Justin Kaeser <jast@zno.de>
2013-05-12 15:09:59 +0200

    copypaste

--- src/main/scala/com/jmcejuela/scalatron/ControlFunction.scala
+++ src/main/scala/com/jmcejuela/scalatron/ControlFunction.scala
@@ -5,26 +5,37 @@ class ControlFunction {
 }
 
 object CommandParser {
+  val react = """React(.+)""".r
   def apply(command: String) = {
-    def splitParam(param: String) = {
-      val segments = param.split('=')
-      if (segments.length != 2)
-        throw new IllegalStateException("invalid key/value pair: "+param)
-      (segments(0), segments(1))
-    }
-
-    val segments = command.split('(')
-    if (segments.length != 2)
-      throw new IllegalStateException("invalid command: "+command)
-
-    val params = segments(1).dropRight(1).split(',')
-    val keyValuePairs = params.map(splitParam).toMap
-    (segments(0), keyValuePairs)
-  }
+    val react(v) = input
+    ("React"
+      (for {
+       p <- v split ','
+       Array(name,value) = p split '='
+    } yield (name,value) ) toMap
+  } 
 }
 
 class ControlFunctionFactory {
   def create = new ControlFunction().respond _
+}
+
+type Coord = (Int,Int)
+
+object Direction {
+  val zero = (0,0)
+  val l = (-1,0)
+  val r = (1,0)
+  val u = (0,1)
+  val d = (0,-1)
+  val lu = (-1,1)
+  val ru = (1,1)
+  val ld = (-1,-1)
+  val rd = (1,-1)
+
+  def apply(c: Coord) = c match {
+    (x,y) =>  
+  }
 }
 
 trait Action {
@@ -33,4 +44,127 @@ trait Action {
 
 object Action {
   def apply(action: Action*) = action.mkString("|")
+}
+
+
+
+// evil reference cheat
+
+case class XY(x: Int, y: Int) {
+    override def toString = x + ":" + y
+
+    def isNonZero = x != 0 || y != 0
+    def isZero = x == 0 && y == 0
+    def isNonNegative = x >= 0 && y >= 0
+
+    def updateX(newX: Int) = XY(newX, y)
+    def updateY(newY: Int) = XY(x, newY)
+
+    def addToX(dx: Int) = XY(x + dx, y)
+    def addToY(dy: Int) = XY(x, y + dy)
+
+    def +(pos: XY) = XY(x + pos.x, y + pos.y)
+    def -(pos: XY) = XY(x - pos.x, y - pos.y)
+    def *(factor: Double) = XY((x * factor).intValue, (y * factor).intValue)
+
+    def distanceTo(pos: XY): Double = (this - pos).length // Phythagorean
+    def length: Double = math.sqrt(x * x + y * y) // Phythagorean
+
+    def stepsTo(pos: XY): Int = (this - pos).stepCount // steps to reach pos: max delta X or Y
+    def stepCount: Int = x.abs.max(y.abs) // steps from (0,0) to get here: max X or Y
+
+    def signum = XY(x.signum, y.signum)
+
+    def negate = XY(-x, -y)
+    def negateX = XY(-x, y)
+    def negateY = XY(x, -y)
+
+    /** Returns the direction index with 'Right' being index 0, then clockwise in 45 degree steps. */
+    def toDirection45: Int = {
+        val unit = signum
+        unit.x match {
+            case -1 =>
+                unit.y match {
+                    case -1 =>
+                        if(x < y * 3) Direction45.Left
+                        else if(y < x * 3) Direction45.Up
+                        else Direction45.UpLeft
+                    case 0 =>
+                        Direction45.Left
+                    case 1 =>
+                        if(-x > y * 3) Direction45.Left
+                        else if(y > -x * 3) Direction45.Down
+                        else Direction45.LeftDown
+                }
+            case 0 =>
+                unit.y match {
+                    case 1 => Direction45.Down
+                    case 0 => throw new IllegalArgumentException("cannot compute direction index for (0,0)")
+                    case -1 => Direction45.Up
+                }
+            case 1 =>
+                unit.y match {
+                    case -1 =>
+                        if(x > -y * 3) Direction45.Right
+                        else if(-y > x * 3) Direction45.Up
+                        else Direction45.RightUp
+                    case 0 =>
+                        Direction45.Right
+                    case 1 =>
+                        if(x > y * 3) Direction45.Right
+                        else if(y > x * 3) Direction45.Down
+                        else Direction45.DownRight
+                }
+        }
+    }
+
+    def rotateCounterClockwise45 = XY.fromDirection45((signum.toDirection45 + 1) % 8)
+    def rotateCounterClockwise90 = XY.fromDirection45((signum.toDirection45 + 2) % 8)
+    def rotateClockwise45 = XY.fromDirection45((signum.toDirection45 + 7) % 8)
+    def rotateClockwise90 = XY.fromDirection45((signum.toDirection45 + 6) % 8)
+
+
+    def wrap(boardSize: XY) = {
+        val fixedX = if(x < 0) boardSize.x + x else if(x >= boardSize.x) x - boardSize.x else x
+        val fixedY = if(y < 0) boardSize.y + y else if(y >= boardSize.y) y - boardSize.y else y
+        if(fixedX != x || fixedY != y) XY(fixedX, fixedY) else this
+    }
+}
+
+
+object XY {
+    /** Parse an XY value from XY.toString format, e.g. "2:3". */
+    def apply(s: String) : XY = { val a = s.split(':'); XY(a(0).toInt,a(1).toInt) }
+
+    val Zero = XY(0, 0)
+    val One = XY(1, 1)
+
+    val Right     = XY( 1,  0)
+    val RightUp   = XY( 1, -1)
+    val Up        = XY( 0, -1)
+    val UpLeft    = XY(-1, -1)
+    val Left      = XY(-1,  0)
+    val LeftDown  = XY(-1,  1)
+    val Down      = XY( 0,  1)
+    val DownRight = XY( 1,  1)
+
+    def fromDirection45(index: Int): XY = index match {
+        case Direction45.Right => Right
+        case Direction45.RightUp => RightUp
+        case Direction45.Up => Up
+        case Direction45.UpLeft => UpLeft
+        case Direction45.Left => Left
+        case Direction45.LeftDown => LeftDown
+        case Direction45.Down => Down
+        case Direction45.DownRight => DownRight
+    }
+
+    def fromDirection90(index: Int): XY = index match {
+        case Direction90.Right => Right
+        case Direction90.Up => Up
+        case Direction90.Left => Left
+        case Direction90.Down => Down
+    }
+
+    def apply(array: Array[Int]): XY = XY(array(0), array(1))
 }
